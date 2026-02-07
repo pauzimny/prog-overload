2c9dd627c857d68e035db0d4491f6882dfebba6a
Paulina Zimny <zimny.paulina@gmail.com>
2026-02-07 21:54:37 +0100

    Google login: Redirect fix

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -33,7 +33,7 @@ export default function AuthPage() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${window.location.origin}/dashboard`,
+          redirectTo: "https://prog-overload.vercel.app/dashboard",
         },
       });
 
